ade365643ceffe52080d6735598d401f4359f62e
KDSBrowne <kert.browne85@gmail.com>
2017-11-05 18:58:41 -0800

    add formatted message description

--- bigbluebutton-html5/imports/ui/components/shortcut-keys/component.jsx
+++ bigbluebutton-html5/imports/ui/components/shortcut-keys/component.jsx
@@ -70,7 +70,7 @@ const intlMessages = defineMessages({
   },
   openHelpShortcut: {
     id: 'app.shortcutKeys.openHelpShortcut',
-    description: '',
+    description: 'key combination to open shortcut key list',
   },
 });
 
