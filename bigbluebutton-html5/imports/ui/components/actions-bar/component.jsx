92059da3538f4d8442e3635b43ffe6f321b482d0
KDSBrowne <kert.browne85@gmail.com>
2018-01-05 10:07:19 -0800

    align action bar buttons for small screens

--- bigbluebutton-html5/imports/ui/components/actions-bar/component.jsx
+++ bigbluebutton-html5/imports/ui/components/actions-bar/component.jsx
@@ -4,22 +4,61 @@ import EmojiSelect from './emoji-select/component';
 import ActionsDropdown from './actions-dropdown/component';
 import AudioControlsContainer from '../audio/audio-controls/container';
 
-const ActionsBar = ({
-  isUserPresenter,
-  emojiList,
-  emojiSelected,
-  handleEmojiChange,
-}) => (
-  <div className={styles.actionsbar}>
-    <div className={styles.left}>
-      <ActionsDropdown {...{ isUserPresenter }} />
-    </div>
-    <div className={styles.center}>
-      <AudioControlsContainer />
-      {/* <JoinVideo /> */}
-      <EmojiSelect options={emojiList} selected={emojiSelected} onChange={handleEmojiChange} />
-    </div>
-  </div>
-);
+export class ActionsBar extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      screenWidth: window.innerWidth,
+    };
+
+    this.handleWindowSizeChange = this.handleWindowSizeChange.bind(this);
+  }
+
+  componentWillMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  handleWindowSizeChange() {
+    this.setState({ screenWidth: window.innerWidth });
+  }
+
+  render() {
+    const {
+      isUserPresenter,
+      emojiList,
+      emojiSelected,
+      handleEmojiChange,
+    } = this.props;
+
+    const { screenWidth } = this.state;
+    const isMobile = screenWidth <= 500;
+
+    return isMobile ?
+      <div className={styles.actionsbar}>
+        <div className={styles.center}>
+          <ActionsDropdown {...{ isUserPresenter }} />
+          <AudioControlsContainer />
+          {/* <JoinVideo /> */}
+          <EmojiSelect options={emojiList} selected={emojiSelected} onChange={handleEmojiChange} />
+          <EmojiSelect options={emojiList} selected={emojiSelected} onChange={handleEmojiChange} />
+        </div>
+      </div>
+      :
+      <div className={styles.actionsbar}>
+        <div className={styles.left}>
+          <ActionsDropdown {...{ isUserPresenter }} />
+        </div>
+        <div className={styles.center}>
+          <AudioControlsContainer />
+          {/* <JoinVideo /> */}
+          <EmojiSelect options={emojiList} selected={emojiSelected} onChange={handleEmojiChange} />
+        </div>
+      </div>;
+  }
+}
 
 export default ActionsBar;
