e4087ff8bc54c9c7689d4ed1617e6637d163f47b
KDSBrowne <kert.browne85@gmail.com>
2018-02-22 08:02:06 -0800

    fetch locale strings

--- bigbluebutton-html5/imports/ui/components/meeting-ended/component.jsx
+++ bigbluebutton-html5/imports/ui/components/meeting-ended/component.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { withRouter } from 'react-router';
 import PropTypes from 'prop-types';
-import { defineMessages, IntlProvider } from 'react-intl';
 import Button from '/imports/ui/components/button/component';
+import Settings from '/imports/ui/services/settings';
 import { styles } from './styles';
 
 const propTypes = {
@@ -12,60 +12,85 @@ const propTypes = {
   }).isRequired,
 };
 
-const language = 'en';
-
-// Usually messages is declared in another file.
-const messages = {
-  'app.meeting.endedMessage': 'You will be forwarded back to the home screen',
-  'app.meeting.ended': 'This session has ended',
-  'app.error.removed': 'You have been removed from the meeting',
-  'app.meeting.endNotification.ok.label': 'OK',
-};
-
-const mesg = defineMessages({
-  endMsg: {
-    id: 'app.meeting.endedMessage',
-    description: 'message to user that they will be redirected to home screen',
-  },
-  ended: {
-    id: 'app.meeting.ended',
-    description: 'meeting has ended message',
-  },
-  kicked: {
-    id: 'app.error.removed',
-    description: 'user was removed from meeting message',
-  },
-  ok: {
-    id: 'app.meeting.endNotification.ok.label',
-    description: 'end screen ok button label',
-  },
-});
+const messageIds = [
+  'app.meeting.endedMessage', // message to user that they will be redirected to home screen
+  'app.meeting.ended', // meeting has ended message
+  'app.error.removed', // user was removed from meeting message
+  'app.meeting.endNotification.ok.label', // end screen ok button label
+];
 
 class MeetingEnded extends React.PureComponent {
-  render() {
-    const { router, code } = this.props;
-    const { intl } = new IntlProvider({ locale: language, messages }, {}).getChildContext();
+  constructor() {
+    super();
 
-    let message;
-    if (code === '403') { message = intl.formatMessage(mesg.kicked); }
-    if (code === '410') { message = intl.formatMessage(mesg.ended); }
+    this.getStringForId = this.getStringForId.bind(this);
+  }
+
+  async componentDidMount() {
+    const url = `/html5client/locale?locale=${Settings.application.locale}`;
+    await fetch(url)
+      .then(async (response) => {
+        const localeJSON = await response.json();
+        const filteredMsgs = Object.keys(localeJSON.messages)
+          .filter(key => messageIds.includes(key))
+          .reduce((obj, key) => {
+            obj[key] = localeJSON.messages[key];
+            return obj;
+          }, {});
+
+        this.setState({ messages: filteredMsgs });
+      });
+  }
+
+  getStringForId(id) {
+    const { messages } = this.state;
+
+    return Object.keys(messages)
+      .filter(key => id.includes(key))
+      .reduce((obj, key) => messages[key], {});
+  }
+
+  render() {
+    if (!this.state) {
+      return (
+        <div className={styles.parent}>
+          <div className={styles.modal} />
+        </div>
+      );
+    }
+
+    const { router, code } = this.props;
+    const { messages } = this.state;
+
+    const okBtnLable = this.getStringForId('app.meeting.endNotification.ok.label');
+    const redirectMsg = this.getStringForId('app.meeting.endedMessage');
+    const msg403 = this.getStringForId('app.meeting.ended');
+    const msg410 = this.getStringForId('app.error.removed');
+
+    let modalTitle;
+    if (code === '403') { modalTitle = msg403; }
+    if (code === '410') { modalTitle = msg410; }
 
     return (
       <div className={styles.parent}>
         <div className={styles.modal}>
-          <div className={styles.content}>
-            <h1 className={styles.title}>{message}</h1>
-            <div className={styles.text}>
-              {intl.formatMessage(mesg.endMsg)}
-            </div>
-            <Button
-              color="primary"
-              className={styles.button}
-              label={intl.formatMessage(mesg.ok)}
-              size="sm"
-              onClick={() => router.push('/logout')}
-            />
-          </div>
+          {
+            this.state && messages && (
+              <div className={styles.content}>
+                <h1 className={styles.title}>{modalTitle}</h1>
+                <div className={styles.text}>
+                  {redirectMsg}
+                </div>
+                <Button
+                  color="primary"
+                  className={styles.button}
+                  label={okBtnLable}
+                  size="sm"
+                  onClick={() => router.push('/logout')}
+                />
+              </div>
+            )
+          }
         </div>
       </div>
     );
