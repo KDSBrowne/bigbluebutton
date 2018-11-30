031083d91616a74a95e6fd9048c799cea6386e21
KDSBrowne <kert.browne85@gmail.com>
2018-11-30 17:14:31 +0000

    fix console CSS class not found errors

--- bigbluebutton-html5/imports/ui/components/user-list/user-list-content/user-polls/component.jsx
+++ bigbluebutton-html5/imports/ui/components/user-list/user-list-content/user-polls/component.jsx
@@ -49,8 +49,8 @@ class UserPolls extends Component {
                 : Session.set('isPollOpen', true);
             }}
           >
-            <Icon iconName="polling" className={styles.icon} />
-            <span className={styles.label} >{intl.formatMessage(intlMessages.pollLabel)}</span>
+            <Icon iconName="polling" />
+            <span>{intl.formatMessage(intlMessages.pollLabel)}</span>
           </div>
         </div>
       </div>
