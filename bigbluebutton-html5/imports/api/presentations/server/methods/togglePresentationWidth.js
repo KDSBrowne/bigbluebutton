fbe2d5e553ba755a2bc9922af27cb18d3bd524ab
KDSBrowne <kert.browne85@gmail.com>
2019-02-14 14:34:29 +0000

    remove message from method

--- bigbluebutton-html5/imports/api/presentations/server/methods/togglePresentationWidth.js
+++ bigbluebutton-html5/imports/api/presentations/server/methods/togglePresentationWidth.js
@@ -1,26 +1,13 @@
-import RedisPubSub from '/imports/startup/server/redis';
 import { check } from 'meteor/check';
 import toggleWidth from '../modifiers/toggleWidth';
 
 export default function togglePresentationWidth(credentials, podId, currentPresentation) {
-  const REDIS_CONFIG = Meteor.settings.private.redis;
-  const CHANNEL = REDIS_CONFIG.channels.toAkkaApps;
-  const EVENT_NAME = 'TogglePresentationWidthPubMsg';
-
-  const { meetingId, requesterUserId } = credentials;
+  const { meetingId } = credentials;
   const { id } = currentPresentation;
 
   check(meetingId, String);
-  check(requesterUserId, String);
   check(id, String);
   check(podId, String);
 
-  const payload = {
-    presentationId: id,
-    podId,
-  };
-
-  toggleWidth(meetingId, podId, id);
-
-  return RedisPubSub.publishUserMessage(CHANNEL, EVENT_NAME, meetingId, requesterUserId, payload);
+  return toggleWidth(meetingId, podId, id);
 }
